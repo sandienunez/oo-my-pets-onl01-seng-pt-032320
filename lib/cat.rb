0bea4a4d8be8b232a2c6db2b8e57793a3ff6c073
Sandie Nunez <snunez@vt.edu>
2020-04-25 22:39:12 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,21 @@
 class Cat
-  # code goes here
+  attr_accessor :owner 
+  attr_reader :name 
+  
+  @@all = []
+  def initialize(name, owner)
+    @name = name 
+    @owner = owner 
+    @mood = "nervous"
+    @@all << self
+  end 
+  
+def self.all 
+  @all 
 end
+
+def self.count 
+  @all 
+end 
+
+end 
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,42 @@
 class Owner
-  # code goes here
-end
+  attr_reader :name, :species
+  @@all = []
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+  def say_species
+    "I am a human."
+  end
+  def self.all 
+    @@all
+  end 
+  def self.count
+    @@all.length 
+  end 
+  def self.reset_all
+    @@all.clear
+  end
+  def cats 
+    Cat.all.select {|cat| cat.owner == self}
+  end
+  def dogs 
+    Dog.all.select {|dog| dog.owner == self}
+  end 
+  def buy_cat(name)
+   Cat.new(name, self)
+  end
+  def buy_dog(name)
+   Dog.new(name, self)
+  end
+  def walk_dogs
+    self.dogs.each {|dog| dog.mood = "happy"}
+  end
+  
+def feed_cats
+    self.cats.each {|dog| dog.mood = "happy"}
+  end
+
+
+end 
